17d79773b8cf491f93be8979a13b7c653d49e5f6
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2020-09-06 22:28:01 +0300

    add scriptDown function

--- 1.action/script.js
+++ 1.action/script.js
@@ -4,15 +4,26 @@ window.onload = function (){
     var topPos = 0;
     var box = document.getElementById("box");
     var actionRight = setInterval(moveRight, 10);
+    var actionDown = setInterval(moveDown, 10);
 
     function moveRight() {
         if(box.style.left == "350px") {
-            clearInterval(action);
+            clearInterval(actionRight);
             return;
         } else {
             leftPos+=1;
             box.style.left = leftPos + "px";
         }
     }
+
+    function moveDown() {
+        if(box.style.top == "350px") {
+            clearInterval(actionDown);
+            return;
+        } else {
+            topPos+=1;
+            box.style.top = topPos + "px";
+        }
+    }   
     
 }
