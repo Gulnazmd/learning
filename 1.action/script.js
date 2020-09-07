34ea3d1af74b82e713735dce430999be5f719a81
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2020-09-07 23:05:01 +0300

    step movement

--- 1.action/script.js
+++ 1.action/script.js
@@ -1,20 +1,39 @@
+    function getClearValue(s){
+        var result = s.substring(0, s.length-2);
+        return result.length == 0 ? 0 : Number.parseInt(result);
+    }
+    function moveX(el, step){
+        var next = getClearValue(el.style.left) + step;
+        if (next > 350 | next < 0){
+            return
+        }
+        el.style.left = next + "px";
+    }
+    function moveY(el, step){
+        var next = getClearValue(el.style.top) + step;
+        if (next > 350 | next < 0){
+            return
+        }
+        el.style.top = next + "px";
+    }
 
     document.addEventListener('keypress', event => {
       var box = document.getElementById("box");
+      var step = 50;
         if (event.keyCode === 87) {
             // up arrow
-            box.style.top = "0px";
+            moveY(box, -step);
         }
         else if (event.keyCode === 83) {
             // down arrow
-            box.style.top = "350px";
+            moveY(box, step);
         }
         else if (event.keyCode === 65) {
            // left arrow
-           box.style.left = "0px";
+           moveX(box, -step);
         }
         else if (event.keyCode === 68) {
            // right arrow
-           box.style.left = "350px";
+           moveX(box, step);
         } 
     });
