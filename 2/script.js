fae730f24863e499375f910a56337dfedccc97de
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2020-09-16 09:32:47 +0300

    button's style.display ("none", "inline")

--- 2/script.js
+++ 2/script.js
@@ -3,20 +3,27 @@ window.onload = function() {
 
     const questions = [
         new question("What do you want to learn?", ["HTML", "JS", "CSS", "C#"], 0),
-        new question("Which words are used in HTML?", ["back", "head", "body", "eyes"], 0)
+        new question("Which words are used in HTML?", ["back", "head", "body", "eyes"], 0),
+        new question("What statments are true for 'String'?",
+                     ["a data type used in programming", "used to represent text", "a data type with 2 possible values", 
+                     "it shows that data value does not exist in the database"], 0)
 
     ];
+
+    Init(questions);
+
     let questionNumber = 0;
 
-    loadQuestion(questionNumber, questions);
-
-    var nextBtn = document.getElementById("control-next")
+    let nextBtn = document.getElementById("control-next")
     nextBtn.addEventListener("click", function(){
-        questionNumber+=1;
-        loadQuestion(questionNumber, questions);
+        if (questionNumber + 1 < questions.length) {
+            questionNumber+=1;
+            loadQuestion(questionNumber, questions);
+        }; 
+        
     });
 
-    var previousBtn = document.getElementById("control-previous")
+    let previousBtn = document.getElementById("control-previous")
     previousBtn.addEventListener("click", function(){
         questionNumber-=1;
         loadQuestion(questionNumber, questions);
@@ -45,14 +52,30 @@ function loadQuestion(number, questions){
         <input type="checkbox" id="answer-choice-${index}" name="contact" value="${index}">
         <label for="answer-choice-${index}">${currentValue}</label>`
     });
+
+    // Если это последний вопрос скрываем Next
+    if (number + 1 == questions.length)
+    {
+        let nextBtn = document.getElementById("control-next");
+        nextBtn.style.display = "none";
+        let finishBtn = document.getElementById("control-finish");
+        finishBtn.style.display = "inline";
+        let previousBtn = document.getElementById("control-previous");
+        previousBtn.style.display = "none"
+
+    } 
 }
-
-
 
 // Вопрос
 function question(text, answers, correctAnswer){
     this.text = text,
     this.answers = answers,
     this.correctAnswer = correctAnswer;
+}
+
+function Init(questions){
+    loadQuestion(0, questions);
+    let finishBtn = document.getElementById("control-finish");
+    finishBtn.style.display = "none";
 }
 
