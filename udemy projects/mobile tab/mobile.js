66ccdfa03b58a921ca3904d89be4c7021a48c999
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2021-02-18 14:56:48 +0300

    add canvas drawing

--- udemy projects/mobile tab/mobile.js
+++ udemy projects/mobile tab/mobile.js
@@ -101,8 +101,8 @@ const game = document.getElementById("game");
 const btnQuestion = document.getElementById("btnQuestion");
 const btnContact = document.getElementById("btnContact")
 const contacts = document.getElementById("contacts");
-const weather = document.getElementById("weather");
-const btnWeather = document.getElementById("btnWeather");
+const drawing = document.getElementById("drawing");
+const btnDraw = document.getElementById("btnDraw");
 const viber = document.getElementById("viber")
 const btnViber = document.getElementById("btnViber");
 
@@ -110,7 +110,7 @@ btnHome.addEventListener("click", () => {
     mainPage.classList.add('show')
     game.classList.remove('show')
     contacts.classList.remove('show')
-    weather.classList.remove('show')
+    drawing.classList.remove('show')
     viber.classList.remove('show')
 })
 
@@ -118,7 +118,7 @@ btnQuestion.addEventListener("click", () => {
     game.classList.add('show')
     mainPage.classList.remove('show')
     contacts.classList.remove('show')
-    weather.classList.remove('show')
+    drawing.classList.remove('show')
     viber.classList.remove('show')
 })
 
@@ -126,12 +126,12 @@ btnContact.addEventListener("click", () => {
     contacts.classList.add('show')
     mainPage.classList.remove('show')
     game.classList.remove('show')
-    weather.classList.remove('show')
+    drawing.classList.remove('show')
     viber.classList.remove('show')
 })
 
-btnWeather.addEventListener("click", () => {
-    weather.classList.add('show')
+btnDraw.addEventListener("click", () => {
+    drawing.classList.add('show')
     contacts.classList.remove('show')
     mainPage.classList.remove('show')
     game.classList.remove('show')
@@ -139,7 +139,7 @@ btnWeather.addEventListener("click", () => {
 })
 btnViber.addEventListener('click', () => {
     viber.classList.add('show')
-    weather.classList.remove('show')
+    drawing.classList.remove('show')
     contacts.classList.remove('show')
     mainPage.classList.remove('show')
     game.classList.remove('show')
@@ -148,6 +148,7 @@ btnViber.addEventListener('click', () => {
 // clock
 
 // clock 
+
 
 const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
@@ -165,15 +166,14 @@ function setTime(){
     const day = time.getDay();
     const date = time.getDate();
     const hours = time.getHours();
-    const hoursForClock = hours % 12;
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hours, 0, 12, 0, 360)}deg)`;
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
     
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}`: minutes}`
+    timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}`: minutes}`
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>` ;
 }
 
@@ -183,4 +183,117 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 setTime()
 
 setInterval(setTime, 1000)
+
+// canvas drawing
+
+const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const clearBtn = document.getElementById('clear');
+const colorEl = document.getElementById('color');
+const sizeEl = document.getElementById('size');
+const ctx = canvas.getContext('2d');
+
+let size = 5;
+let color = 'black'
+let x
+let y
+let isPressed = false
+ 
+// touch devices
+
+canvas.addEventListener("touchstart", function (e) {
+    isPressed = true
+    var touch = e.touches[0];
+        x = touch.clientX
+        y = touch.clientY
+});
+
+canvas.addEventListener("touchend", function (e) {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
+canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    if(isPressed){
+        x2 = touch.clientX
+        y2 = touch.clientY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2 
+    }
+    })
+
+// mouse evens
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed){
+        x2 = e.offsetX
+        y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    } 
+})
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill()
+}
+function drawLine(x1, y1, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
+}
+// btns
+
+function updateSizeEl() {
+    sizeEl.innerText = size
+}
+increaseBtn.addEventListener('click', () => {
+    size += 5
+    if(size > 30) {
+        size = 30
+    }
+    updateSizeEl()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+    if(size < 5) {
+        size = 5
+    }
+    updateSizeEl()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value )
+clearBtn.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
+
 
